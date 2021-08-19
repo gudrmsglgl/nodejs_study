00ffedc87cff8d1a3a54571f5cc985cd20492031
gudrmsglgl <gudrmsglgl@gmail.com>
2021-08-19 22:55:53 +0900

    feat(module): export func && import

---
+++ 5-module/app.js
@@ -0,0 +1,15 @@
+// import counter from './counter.js';
+
+// increase();
+// increase();
+// console.log(getCount());
+
+import * as counter from './counter.js';
+
+counter.increase();
+counter.increase();
+counter.increase();
+
+console.log(
+    counter.getCount()
+);
